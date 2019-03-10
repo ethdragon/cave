70445efcc9f2c6718fa1f646f38e14b5410c8e7e
Yang Liu <ethdragon@gmail.com>
2019-03-09 19:38:03 -0500

    init commit

---
+++ handler.ts
@@ -0,0 +1,13 @@
+
+export const hello = async (event: any, _: any) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
