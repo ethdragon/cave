98a17c9b78795dfb6dd5ba2114f6b8bf118e0458
ethdragon <ethdragon@gmail.com>
2019-03-16 15:35:03 -0400

    make a deployable version (#1)
    * make a deployable version
    
    * lint
    
    * some function
    
    * tslint
    
    * tsjest
    
    * travis
    
    * change cache
    
    * linting

--- handler.ts
+++ handler.ts
@@ -1,13 +1,1 @@
-
-export const hello = async (event: any, _: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+export * from './src/tesla';
