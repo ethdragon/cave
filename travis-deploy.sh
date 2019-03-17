50e8dd6af08141f0b0116bc025e7e0a7d5cc817c
Yang Liu <ethdragon@gmail.com>
2019-03-16 21:42:37 -0400

    ci

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,9 +3,5 @@ if [ "${TRAVIS_BRANCH}" != "master" ] || [ "${TRAVIS_PULL_REQUEST}" != "false" ]
 then
   echo "NOT deploying: Deploying only on master, but this is $TRAVIS_BRANCH."
 else
-  sls deploy --stage prod --force
+  sls deploy --stage prod --verbose
 fi
-
-# sls package --package dist/.serverless
-# ls -la dist/.serverless
-sls deploy --stage prod --force --verbose
