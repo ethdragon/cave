3ffec75bbbf757ed7daccc1788209739d1e9abd7
ethdragon <ethdragon@gmail.com>
2019-03-16 22:05:25 -0400

    cons (#5)
    consistent serverless version

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" != "master" ]
+if [ "${TRAVIS_BRANCH}" != "master" ] || [ "${TRAVIS_PULL_REQUEST}" != "false" ]
 then
   echo "NOT deploying: Deploying only on master, but this is $TRAVIS_BRANCH."
 else
-  sls deploy --stage prod --force
+  sls deploy --stage prod --verbose
 fi
