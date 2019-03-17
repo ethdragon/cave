0a482777a75a55822434144614a1b9dde94d5d50
Yang Liu <ethdragon@gmail.com>
2019-03-16 21:21:31 -0400

    m

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,6 +6,6 @@ else
   sls deploy --stage prod --force
 fi
 
-sls package --package dist/.serverless
-ls -la dist/.serverless
+# sls package --package dist/.serverless
+# ls -la dist/.serverless
 sls deploy --stage prod --force --verbose --package dist
